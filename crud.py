d26edfc0e8758e8663c7e82498ef482b8242204d
David Sapiro <dsap48@gmail.com>
2024-10-23 17:04:06 -0700

    refactored CRUD functions to be model specific. get_item, get_items, create_item, and update_item are all functional

--- crud.py
+++ crud.py
@@ -1,31 +1,33 @@
 from sqlalchemy.orm import Session
+#from fastapi.encoders import jsonable_encoder
 from . import models
 from . import schemas
 
-def get_all(db: Session, model: models):
-    return db.query(model).all().sort()
+def get_items(db: Session):
+    return db.query(models.Item).all()
 
-def get_one(db: Session, model: models, id: int):
-    return db.query(model).filter(model.id == id).first()
+def get_item(db: Session, id: int):
+    return db.query(models.Item).filter(models.Item.id == id).first()
 
-def create(db: Session, item: schemas.ItemCreate):
+def create_item(db: Session, item: schemas.ItemCreate):
     db_item = models.Item(**item.model_dump()) # turns dict into a model instance
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
 
-def update(db: Session, id: int, model: models, data: schemas):
-    db_item = db.query(model).filter(model.id == id).first()
+def update_item(db: Session, id: int, item: schemas.Item):
+    db_item = db.query(models.Item).filter(models.Item.id == id).first()
 
     if db_item is None:
         return None
 
-    db_item = {
-        "item": data.name,
-        "description": data.description,
-        "measurements": data.measurements
-    }
+    # for key, value in item.model_dump(exclude_unset=True).items():
+    #     setattr(db_item, key, value)
+
+    db_item.name = item.name
+    db_item.description = item.description
+    db_item.measurements = item.measurements
 
     db.commit()
     db.refresh(db_item)
