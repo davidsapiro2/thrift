36be2d1c4662fe859ba37c30eaa7de2a2040f69c
David Sapiro <dsap48@gmail.com>
2024-10-23 17:05:06 -0700

    updated routes for new CRUD function names and finalized update_item route

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine
-from .crud import create, get_all, get_one, update
+from . import crud
 from . import schemas
 from . import models
 
@@ -25,14 +25,14 @@ def get_db():
 def get_all_items(db: Session = Depends(get_db)):
     """Returns list of all items"""
 
-    items = get_all(db=db, model=models.Item)
+    items = crud.get_items(db=db)
     return items
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_model=schemas.Item)
 def get_one_item(item_id: int, db: Session = Depends(get_db)):
     """Returns details of one item"""
 
-    item = get_one(db=db, model=models.Item, id=item_id)
+    item = crud.get_item(db=db, id=item_id)
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item
@@ -41,14 +41,14 @@ def get_one_item(item_id: int, db: Session = Depends(get_db)):
 def add_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     """Creates a new item"""
 
-    new_item = create(item=item, db=db)
+    new_item = crud.create_item(item=item, db=db)
     return new_item
 
 @app.put("/items/{item_id}", response_model=schemas.Item)
 def update_item(item_id: int, item: schemas.Item, db: Session = Depends(get_db)):
     """Updates an item"""
 
-    updated_item = update(db=db, model=models.Item, id=item_id, data=item)
+    updated_item = crud.update_item(db=db, id=item_id, item=item)
     if updated_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return updated_item
